632c80a933d84e2c9fc2e2066bcc17fb8c0f4d2e
MirceaGheorghiu <110449045+MirceaGheorghiu@users.noreply.github.com>
2022-11-23 14:10:11 +0200

    JS-m01 s01.ex03-06; s02.ex01,03

---
+++ m01s02/ex01/js/shapes.js
@@ -0,0 +1,10 @@
+console.warn(`Aria unui patrat cu latura 16 este calculata mai jos: `);
+console.log(Math.pow(16, 2).toString());
+
+console.warn(
+  'Aria unui dreptunghi cu latura 20 si latimea 12 este calculata mai jos:',
+);
+console.log((20 * 12).toString());
+
+console.warn(`Lungimea totala a laturilor unui dreptunghi de 15 pe 38 este:`);
+console.log((15 * 2 + 38 * 2).toString());
