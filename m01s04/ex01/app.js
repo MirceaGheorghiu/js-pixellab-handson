795fd0da146d697d6abef5ab69d3a8c33c8aca4b
MirceaGheorghiu <110449045+MirceaGheorghiu@users.noreply.github.com>
2022-11-29 21:56:13 +0200

    curs

---
+++ m01s04/ex01/app.js
@@ -0,0 +1,14 @@
+var number = prompt('Introdu un numar:');
+var message = '';
+var messageElement = document.getElementById('message');
+
+document.getElementById('message');
+
+if (number % 2 === 0) {
+  message = 'Numarul este par';
+} else {
+  message = 'Numarul este par';
+}
+
+console.log(message);
+messageElement.innerText = message;
