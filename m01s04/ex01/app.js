31d59e25f3d0cc1419e20605d7dc280ac582fb18
MirceaGheorghiu <110449045+MirceaGheorghiu@users.noreply.github.com>
2022-12-02 16:05:01 +0200

    curs

--- m01s04/ex01/app.js
+++ m01s04/ex01/app.js
@@ -1,13 +1,22 @@
-var number = prompt('Introdu un numar:');
+var userInput = prompt('Introdu un numar:');
+var referenceNumber = 20;
+// se poate pune un prompt + validare si pt acest prompt
 var message = '';
 var messageElement = document.getElementById('message');
+var number = Number(userInput);
+// number poate fi un numar sau Nan
+// isNaN(number) || userInput === null
 
-document.getElementById('message');
-
-if (number % 2 === 0) {
-  message = 'Numarul este par';
+if (userInput === null || userInput.trim().length === 0 || isNaN(number)) {
+  message = 'Nu ai introdus un numar';
 } else {
-  message = 'Numarul este par';
+  if (number > referenceNumber) {
+    message = `Numarul este mai mare decat ${referenceNumber}`;
+  } else if (number === referenceNumber) {
+    message = `Numarul este egal cu ${referenceNumber}`;
+  } else {
+    message = `Numarul este mai mic decat ${referenceNumber}`;
+  }
 }
 
 console.log(message);
