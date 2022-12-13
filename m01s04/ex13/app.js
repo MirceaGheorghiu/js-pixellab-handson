4632641e2251a14a72e08503b791662506ef5aaa
MirceaGheorghiu <mircea.r.gheorghiu@gmail.com>
2022-12-13 21:56:44 +0200

    curs

---
+++ m01s04/ex13/app.js
@@ -0,0 +1,15 @@
+var globalVariable = 'Ma aflu in contextul global';
+
+if (true) {
+  var localVariable = 'Ma aflu in contextul local';
+  var globalVariable = 'Nu ma aflu in contextul global';
+
+  console.log(globalVariable);
+  console.log(localVariable);
+}
+
+console.log(localVariable);
+
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
