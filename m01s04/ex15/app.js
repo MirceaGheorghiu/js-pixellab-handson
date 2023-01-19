9012af9e8271f0b97157abd1671b5c46d83482c5
MirceaGheorghiu <110449045+MirceaGheorghiu@users.noreply.github.com>
2023-01-19 21:58:45 +0200

    curs

--- m01s04/ex15/app.js
+++ m01s04/ex15/app.js
@@ -34,13 +34,18 @@ var person = {
 
 // high school flashback :))
 for (let i = 0; i < person.friends.length; i++) {
-  for (let j = 0; j < person.friends.length; j++) {
-    let diff = Math.abs(person.friends[i].age - person.friends[j].age);
+  const outerFriend = person.friends[i];
 
-    if (diff !== 0) {
-      console.log(
-        `Intre ${person.friends[i].name} si ${person.friends[j].name} este o diferenta de ${diff} ani.`,
-      );
+  for (let j = 0; j < person.friends.length; j++) {
+    const innerFriend = person.friends[j];
+    let diff = Math.abs(outerFriend.age - innerFriend.age);
+
+    if (i === j) {
+      continue;
     }
+
+    console.log(
+      `Intre ${outerFriend.name} si ${innerFriend.name} este o diferenta de ${diff} ani.`,
+    );
   }
 }
